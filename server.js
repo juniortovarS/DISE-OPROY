d90a073e37056761cafcec9f168d13d87d995216
juniortovarS <juniortovar601@gmail.com>
2026-04-16 17:00:55 -0500

    Actualización dependencias y server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
